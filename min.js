bdd0c2f47a4250b012b71e732c51618428a75c68
ajacos <acosta.aaron@psdqatar.com>
2023-11-20 22:05:24 +0300

    added 1 new file and changed most of the files

--- min.js
+++ min.js
@@ -43,6 +43,91 @@ document.getElementById("submit").addEventListener("click", e=>{
 
 
 document.getElementById('10rst').addEventListener('click', e => {
+    function shuffleWithoutRepetition(array) {
+        var i, j, t;
+        for (i = 0; i < array.length; i++) {
+          j = Math.floor(Math.random() * array.length);
+          if (j != i) {
+            t = array[i];
+            array[i] = array[j];
+            array[j] = t;
+          } else {
+            while (j == i) {
+              j = Math.floor(Math.random() * array.length);
+            }
+            t = array[i];
+            array[i] = array[j];
+            array[j] = t;
+          }
+        }
+        return array;
+      }
+
+    fetch("g10.json")
+    .then(function(response) {
+      return response.json(); // parse the JSON file
+    })
+    .then(function(data) {
+      let 
+      arc = data.archimedes,
+      ber = data.bernoulli,
+      edi = data.edison,
+      ein = data.einstein,
+      far = data.faraday,
+      max = data.maxwell,
+      newt = data.newton,
+      pas = data.pascal,
+      tho = data.thomson,
+      g10 = []
+
+      
+      let 
+      arc1 = shuffleWithoutRepetition(arc).splice(0,20),
+      ber1 = shuffleWithoutRepetition(ber).slice(0, 19)
+      edi1 = shuffleWithoutRepetition(edi).slice(0, 20),
+      ein1 = shuffleWithoutRepetition(ein).slice(0, 21)
+      far1 = shuffleWithoutRepetition(far).slice(0, 20)
+      max1 = shuffleWithoutRepetition(max).slice(0, 21),
+      newt1 = shuffleWithoutRepetition(newt).slice(0, 20),
+      pas1 = shuffleWithoutRepetition(pas).slice(0, 20),
+      tho1 = shuffleWithoutRepetition(tho).slice(0, 21),
+      final = []
+
+      g10 = g10.concat(arc1).concat(ber1).concat(edi1).concat(ein1).concat(far1).concat(max1).concat(newt1).concat(pas1).concat(tho1)
+      //g10.push(ber1)
+
+      for(i = 0; i < g10.length; i++) {
+        d = i+1
+        //fina.pop();
+        //g10[i] = '<tr><td class="thin">' + d + "</td><td>" + g10[i] + "</td></tr>"
+        if(i <= 19) {
+            g10[i] = '<tr><td class="thin">' + d + "</td><td>" + g10[i] + "</td><td class='sec'>Archimedes</td></tr>"
+        } else if (i <= 38 ) {
+            g10[i] = '<tr><td class="thin">' + d + "</td><td>" + g10[i] + "</td><td class='sec'>Bernoulli</td></tr>"
+        } else if (i <= 58 ) {
+            g10[i] = '<tr><td class="thin">' + d + "</td><td>" + g10[i] + "</td><td class='sec'>Edison</td></tr>"
+        } else if (i <= 79 ) {
+            g10[i] = '<tr><td class="thin">' + d + "</td><td>" + g10[i] + "</td><td class='sec'>Einstein</td></tr>"
+        } else if (i <= 99 ) {
+            g10[i] = '<tr><td class="thin">' + d + "</td><td>" + g10[i] + "</td><td class='sec'>Faraday</td></tr>"
+        } else if (i <= 120 ) {
+            g10[i] = '<tr><td class="thin">' + d + "</td><td>" + g10[i] + "</td><td class='sec'>Maxwell</td></tr>"
+        } else if (i <= 140 ) {
+            g10[i] = '<tr><td class="thin">' + d + "</td><td>" + g10[i] + "</td><td class='sec'>Newton</td></tr>"
+        } else if (i <= 160 ) {
+            g10[i] = '<tr><td class="thin">' + d + "</td><td>" + g10[i] + "</td><td class='sec'>Pascal</td></tr>"
+        } else if (i <= 181 ) {
+            g10[i] = '<tr><td class="thin">' + d + "</td><td>" + g10[i] + "</td><td class='sec'>Thomson</td></tr>"
+        }
+
+      }
     
+      document.getElementById("textn").innerHTML = '<tr><th class="thin">No.</th><th>Name:</th><th class="sec">Section:</th></tr>' + g10.join(''),
+      document.getElementById("textp").innerHTML = ""
+      document.getElementById("addbtn").innerHTML = ""
+      
+
+        
+    })
 })
 // By Aj Acosta
